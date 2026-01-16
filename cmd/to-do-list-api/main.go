a1b8967f6d0d1d7fea393aff0431732e8253a62a
gabrielssssssssss <974gabriel.contact@gmail.com>
2026-01-16 12:35:56 +0100

    update: get environnement variable from .env file

--- cmd/to-do-list-api/main.go
+++ cmd/to-do-list-api/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/gabrielssssssssss/todo-list-api/config"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	client, err := config.NewPostgresDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(client.Stats())
 }
