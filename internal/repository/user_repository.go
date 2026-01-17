9486521ebf4ba3a4d426ef18d6f44067dbf66ab2
gabrielssssssssss <974gabriel.contact@gmail.com>
2026-01-17 15:06:02 +0100

    create: password helper for hash and verify password

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -2,9 +2,13 @@ package repository
 
 import (
 	"database/sql"
+
+	"github.com/gabrielssssssssss/todo-list-api/internal/entity"
+	"github.com/gabrielssssssssss/todo-list-api/internal/model"
 )
 
 type UserRepository interface {
+	AddUser(*entity.UserEntity) (*model.UserModel, error)
 }
 
 type userRepositoryImpl struct {
