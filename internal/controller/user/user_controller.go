d464633f3b444ae15f46d144acea15950a0a6424
gabrielssssssssss <974gabriel.contact@gmail.com>
2026-01-17 15:22:18 +0100

    update: user controller & router

--- internal/controller/user/user_controller.go
+++ internal/controller/user/user_controller.go
@@ -1,1 +1,39 @@
 package user
+
+import (
+	"net/http"
+
+	"github.com/gabrielssssssssss/todo-list-api/internal/model"
+	"github.com/gabrielssssssssss/todo-list-api/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserService service.UserService
+}
+
+func NewUserController(UserService *service.UserService) UserController {
+	return UserController{UserService: *UserService}
+}
+
+func (controller *UserController) AddUser(c *gin.Context) {
+	var request = model.UserModel{}
+
+	err := c.ShouldBindBodyWithJSON(&request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		c.Abort()
+	}
+
+	response, err := controller.UserService.AddUser(&request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		c.Abort()
+	}
+
+	c.JSON(http.StatusOK, response)
+}
--- internal/controller/user/user_router.go
+++ internal/controller/user/user_router.go
@@ -1,1 +1,9 @@
 package user
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (controller *UserController) Route(rg *gin.RouterGroup) {
+	rg.POST("/todos", controller.AddUser)
+}
